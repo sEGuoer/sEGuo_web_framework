2eeebf39eed9e6e1f872f6a4a8d1e2db3d73dfb2
sEGuoer <709074535@qq.com>
2023-09-20 11:00:24 +0800

    servlet运行顺序(完整生命周期,先不考虑filter和session):
    listener->构造器->init->service(可以多次)->destroy

---
+++ D20230915-spring-quickstart/src/test/java/D20230919/ServletTest.java
@@ -0,0 +1,11 @@
+package D20230919;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ServletTest {
+    @Test
+    void lifeCycle(){
+        Assertions.assertNotNull("listener->构造器->init->service(可以多次)->destroy");
+    }
+}
