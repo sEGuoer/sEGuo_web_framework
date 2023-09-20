1424218f84d8b41c1cee2fc020b8ad7859bd9251
sEGuoer <709074535@qq.com>
2023-09-20 16:31:43 +0800

    initAndDestroyMethod

--- sEGuo-framework/src/main/java/com/sEGuo/sevice/impl/UserSeviceSimple.java
+++ sEGuo-framework/src/main/java/com/sEGuo/sevice/impl/UserSeviceSimple.java
@@ -3,8 +3,6 @@ package com.sEGuo.sevice.impl;
 import com.sEGuo.sevice.UserSevice;
 import com.sEGuo.dao.UserDao;
 import com.sEGuo.pojo.User;
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 
 public class UserSeviceSimple implements UserSevice {
     protected UserDao userSimple;
@@ -13,12 +11,18 @@ public class UserSeviceSimple implements UserSevice {
         this.userSimple = userSimple;
     }
 
+    public void init() {
+        System.out.println("UserSeviceSimple.init");
+    }
+
+    public void destroy() {
+        System.out.println("UserSeviceSimple.destroy");
+    }
 
 
-
-    public String userLogin(String email, String pwd){
-        User user = userSimple.getUserByEmail(email,pwd);
-        return "email="+user.getEmail()+" password="+user.getPassword();
+    public String userLogin(String email, String pwd) {
+        User user = userSimple.getUserByEmail(email, pwd);
+        return "email=" + user.getEmail() + " password=" + user.getPassword();
     }
 
 
--- sEGuo-framework/src/test/java/BeforeSpringTest.java
+++ sEGuo-framework/src/test/java/BeforeSpringTest.java
@@ -1,5 +1,6 @@
 import com.sEGuo.dao.UserDao;
 import com.sEGuo.pojo.User;
+import com.sEGuo.sevice.UserSevice;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
@@ -12,5 +13,15 @@ public class BeforeSpringTest {
         ApplicationContext ac=new ClassPathXmlApplicationContext("bean.xml");
         UserDao userDao1 = (UserDao)ac.getBean("UserDao");
         Assertions.assertNotNull(userDao1);
+    }
+    @Test
+    void initAndDestroyMethodTest(){
+        ApplicationContext ac=new ClassPathXmlApplicationContext("bean.xml");
+        UserSevice userSevice =(UserSevice) ac.getBean("UserSevice");
+        Assertions.assertNotNull(userSevice);
+        /*ac.close();
+        * 此时就会显示出destroy的方法的输出结果
+        * */
+
     }
 }
